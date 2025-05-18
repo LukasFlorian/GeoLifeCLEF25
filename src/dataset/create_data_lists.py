f003e15472c7b85ab31fe55e87d9eca982dea41a
LukasFlorian <Lukas.richter.1@gmx.net>
2025-05-18 18:48:40 +0200

    data preprocessing

--- src/dataset/create_data_lists.py
+++ src/dataset/create_data_lists.py
@@ -5,7 +5,19 @@ Module for creating data lists for the GLC25 dataset.
 import os
 import json
 
-def save_image_list(labels, image_paths, file_path, split: str = "train"):
+def save_image_list(labels: list[list[int]], image_paths: list[str], file_path, split: str = "train"):
+    """
+    Saves the image list and corresponding labels to JSON files.
+
+    Args:
+        labels (_type_): _description_
+        image_paths (_type_): _description_
+        file_path (_type_): _description_
+        split (str, optional): _description_. Defaults to "train".
+
+    Raises:
+        ValueError: _description_
+    """
     split = split.lower()
     if split not in ["train", "val", "test"]:
         raise ValueError("Invalid split. Must be 'train', 'val', or 'test'.")
@@ -17,3 +29,5 @@ def save_image_list(labels, image_paths, file_path, split: str = "train"):
         json.dump(labels, f)
     with open(image_file, "w", encoding="utf-8") as f:
         json.dump(image_paths, f)
+    print(f"Saved {split} data lists containing {len(labels)} data points to {file_path}")
+
